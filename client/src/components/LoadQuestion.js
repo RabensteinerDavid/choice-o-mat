d1ec154f1140bcd608b362944dc1e8db128daa13
Rabensteiner David <rabendavid02@gmail.com>
2024-05-14 00:32:15 +0200

    change port settings for hot reload error

--- client/src/components/LoadQuestion.js
+++ client/src/components/LoadQuestion.js
@@ -1,72 +1,47 @@
 import { useEffect, useState } from 'react'
 import { getAllQuestion, getMaxPage } from '../api'
 
-export const loadQuestions = selectedType => {
-  const [questions, setQuestions] = useState([])
+export const loadQuestions = async selectedType => {
+  var filteredQuestions
+  try {
+    const response = await getAllQuestion()
+    const fetchedQuestions = response.data.data
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getAllQuestion()
-        const fetchedQuestions = response.data.data
+    filteredQuestions = fetchedQuestions.filter(
+      question => question.type === selectedType
+    )
+  } catch (error) {
+    console.error('Error fetching questions:', error)
+  }
 
-        const filteredQuestions = fetchedQuestions.filter(
-          question => question.type === selectedType
-        )
-        setQuestions(filteredQuestions)
-      } catch (error) {
-        console.error('Error fetching questions:', error)
-      }
-    }
-
-    fetchData()
-  }, [selectedType])
-
-  return questions
+  return filteredQuestions
 }
 
 export const loadQuestionType = (questions, version) => {
-  const [question, setQuestion] = useState(null)
+  var questionData
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (questions.length > 0) {
-        const questionData = questions[version]
-        setQuestion(questionData)
-      }
-    }
-    fetchData()
-  }, [questions, version])
+  if (questions.length > 0) {
+    questionData = questions[version]
+  }
 
-  return question
+  return questionData
 }
 
-export const loadQuestionsID = id => {
-  const [questions, setQuestions] = useState([])
+export const loadQuestionsID = async id => {
+  var filteredQuestions
+  try {
+    const response = await getAllQuestion()
+    const fetchedQuestions = (response.data.data = fetchedQuestions.filter(
+      question => question._id === id
+    ))
+  } catch (error) {
+    console.error('Error fetching questions:', error)
+  }
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getAllQuestion()
-        const fetchedQuestions = response.data.data
-
-        const filteredQuestions = fetchedQuestions.filter(
-          question => question._id === id
-        )
-        setQuestions(filteredQuestions)
-      } catch (error) {
-        console.error('Error fetching questions:', error)
-      }
-    }
-
-    fetchData()
-  }, [id])
-
-  return questions
+  return filteredQuestions
 }
 
 export const loadQuestionsOrderID = async () => {
-  const [questionsID, setQuestionsID] = useState([])
   try {
     const response = await getAllQuestion()
     const fetchedQuestions = response.data.data
@@ -74,15 +49,13 @@ export const loadQuestionsOrderID = async () => {
     const sortedQuestions = fetchedQuestions.sort((a, b) => a.page - b.page)
     const questionIds = sortedQuestions.map(question => question._id)
 
-    setQuestionsID(questionIds)
-    return questionsID
+    return questionIds
   } catch (error) {
     console.error('Error fetching questions:', error)
     return []
   }
 }
 export const loadQuestionsOrderData = async () => {
-
   try {
     const response = await getAllQuestion()
     const fetchedQuestions = response.data.data
@@ -96,11 +69,10 @@ export const loadQuestionsOrderData = async () => {
 
 export const getMaxPageValue = async () => {
   try {
-    const data = await getMaxPage();
-    console.log(data)
-    return data.data.maxPage;
+    const data = await getMaxPage()
+    return data.data.maxPage
   } catch (error) {
-    console.error('Error:', error);
-    return null; 
+    console.error('Error:', error)
+    return null
   }
-};
+}
