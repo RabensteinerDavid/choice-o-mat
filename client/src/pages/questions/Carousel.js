931fecf0c3069d4404ca39f4c5f8114c73dd1ff7
Rabensteiner David <rabendavid02@gmail.com>
2024-05-23 15:48:28 +0200

    add lottie file to carousel

--- client/src/pages/questions/Carousel.js
+++ client/src/pages/questions/Carousel.js
@@ -4,6 +4,8 @@ import FotBar from '../../components/FotBar'
 import '../../style/questions/carousel.css'
 import HeadingQuestion from '../../components/HeadingQuestion'
 import CarouselImage from '../../components/questions_images/CarouselImage'
+import { Player } from '@lottiefiles/react-lottie-player';
+import SelectionImage from '../../components/questions_images/SelectionImage'
 
 const Carousel = ({ question, pageNumber, maxPage }) => {
   
@@ -17,12 +19,23 @@ const Carousel = ({ question, pageNumber, maxPage }) => {
           <React.Fragment>
             <HeadingQuestion heading={heading} subheading={subheading} />
             <div>
-              {answers.map(answer => (
+            {answers.map(answer => (
                 <div key={answer._id}>
                   <p>{answer.text}</p>
                   <p>Points DA: {answer.points.da}</p>
                   <p>Points MTD: {answer.points.mtd}</p>
-                  <CarouselImage photo={answer.photo} />
+                  {answer.photo && answer.photo.includes("json") && (
+                    <Player
+                      src={`http://localhost:3001/lottie/${answer.photo}`}
+                      className='player'
+                      loop
+                      autoplay
+                      style={{ height: '300px', width: '300px' }}
+                    />
+                  )}
+                  {answer.photo && !answer.photo.includes("json") && (
+                      <SelectionImage photo={answer.photo} />
+                  )}
                 </div>
               ))}
             </div>
