8d0f35cee8298216289b0926a488331603752952
julis <s2210238053@fhooe.at>
2024-05-26 23:55:40 +0200

    carousel updated

--- client/src/pages/questions/Carousel.js
+++ client/src/pages/questions/Carousel.js
@@ -5,25 +5,41 @@ import "../../style/questions/carousel.css";
 import HeadingQuestion from "../../components/HeadingQuestion";
 import { Player } from "@lottiefiles/react-lottie-player";
 import SelectionImage from "../../components/questions_images/SelectionImage";
+//swiper imports
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from 'swiper';
+import useWindowDimensions from "../../components/useWindowSize";
 
 const Carousel = ({ question, pageNumber, maxPage }) => {
   const { heading, subheading, answers } = question;
 
-  const [focusedButtons, setFocusedButtons] = useState([]); // State für die fokussierten Buttons
+  const [focusedButtons, setFocusedButtons] = useState([]); 
 
-  // const [swiperControl, setSwiperControl] = useState(null); //NEU
+  const { height, width } = useWindowDimensions()
 
   const toggleFocus = (id) => {
     setFocusedButtons(
       (prevState) =>
         prevState.includes(id)
-          ? prevState.filter((buttonId) => buttonId !== id) // Entferne den Fokus, wenn der Button bereits fokussiert ist
-          : [...prevState, id] // Füge den Fokus hinzu, wenn der Button nicht fokussiert ist
+          ? prevState.filter((buttonId) => buttonId !== id) 
+          : [...prevState, id] 
     );
+  };
+
+  const getSlidesPerView = () => {
+    if (window.innerWidth <= 400) return 1.5;
+    if (window.innerWidth <= 650) return 2.5;
+    return 3.5;
+  };
+
+  const getSpaceBetween = () => {
+    if (window.innerWidth <= 650) return 20;
+    if (window.innerWidth <= 768) return 28;
+    if (window.innerWidth <= 992) return 34;
+    if (window.innerWidth <= 1200) return 38;
+    return 50;
   };
 
   return (
@@ -36,8 +52,8 @@ const Carousel = ({ question, pageNumber, maxPage }) => {
             <>
               <Swiper 
                 watchSlidesProgress={true}
-                slidesPerView={3.5}
-                spaceBetween={60}
+                slidesPerView={getSlidesPerView()}
+                spaceBetween={getSpaceBetween()}
                 autoplay={{ stopOnLastSlide: false}}
                 loop={true}
                 navigation={true}
@@ -72,6 +88,8 @@ const Carousel = ({ question, pageNumber, maxPage }) => {
                   </SwiperSlide>
                 ))}
               </Swiper>
+              <div>{width}</div>
+              <div>{height}</div>
             </>
           </React.Fragment>
         ) : (
