5af101addd4bba47411fcb44ddbec3034f075fa9
Rabensteiner David <rabendavid02@gmail.com>
2024-05-27 23:42:30 +0200

    add fox with eye tracker

--- client/src/components/Arrows.jsx
+++ client/src/components/Arrows.jsx
@@ -1,32 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useHistory, useNavigate, useParams } from 'react-router-dom'
+import React, { useEffect, useRef, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import '../style/links.css'
 import { getMaxPageValue } from './LoadQuestion'
-import useWindowDimensions from './useWindowSize'
+import { Player } from '@lottiefiles/react-lottie-player'
+import Eyes from './Eyes'
 
 const Arrows = ({ prevQuestion, nextQuestion, saveAnswers }) => {
-  const navigation = useNavigate()
+  const navigate = useNavigate()
   const [maxPageSide, setMaxPageSide] = useState(0)
   const { id: page_id } = useParams()
+  const maxPageSideRef = useRef(0)
+  const [fadeClass, setFadeClass] = useState('fade-in')
 
   useEffect(() => {
     const fetchMaxPageSide = async () => {
       const maxPageValue = await getMaxPageValue()
       setMaxPageSide(maxPageValue)
+      maxPageSideRef.current = maxPageValue
     }
 
     fetchMaxPageSide()
   }, [])
 
   const handleNavigation = questionId => {
-    saveAnswers()
-    setTimeout(() => {
-      navigation(`/questions/${questionId}`)
-    }, 0)
+    if (page_id != 1) {
+      setFadeClass('fade-out')
+      saveAnswers()
+      navigate(`/questions/${questionId}`)
+    }
+  }
+
+  const handleNavigationButton = questionId => {
+    if (page_id != 1) {
+      setFadeClass('fade-out')
+      saveAnswers()
+      navigate(`/questions/${questionId}`)
+    }
   }
 
   const isValidQuestionId = questionId => {
-    return questionId > 0 && questionId < maxPageSide
+    return questionId > 0 && questionId <= maxPageSideRef.current +1
   }
 
   return (
@@ -39,20 +52,62 @@ const Arrows = ({ prevQuestion, nextQuestion, saveAnswers }) => {
           {'<'}
         </span>
       )}
+      <div className='anim-border'></div>
 
-      <div className='navigation-position'>
-        <div className='navigatio-item visited'>1</div>
-        {maxPageSide &&
+      <div className={`navigation-position ${fadeClass}`}>
+        {page_id == 1 && (
+          <div>
+            <Eyes />
+            <Player
+              src={`http://localhost:3001/lottie/test.json`}
+              className='fox-nav'
+              loop
+              autoplay
+              style={{
+                height: '100px',
+                width: '100px',
+                marginTop: '-100px',
+                marginLeft: '-30px'
+              }}
+            />
+          </div>
+        )}
+        <div
+          className='navigatio-item visited'
+          onClick={() => handleNavigationButton(1)}
+        >
+          1
+        </div>
+        {maxPageSide > 1 &&
           Array.from({ length: maxPageSide - 1 }).map((_, index) => (
-            <React.Fragment key={index}>
+            <React.Fragment key={index + 1}>
               <div
                 className={`navigatio-item-bridge${
-                  index < page_id ? ' visited' : ''
+                  index + 1 < page_id ? ' visited' : ''
                 }`}
               ></div>
-
+              {page_id == index + 2 && (
+                <div>
+                  <Eyes />
+                  <Player
+                    src={`http://localhost:3001/lottie/test.json`}
+                    className='fox-nav'
+                    loop
+                    autoplay
+                    style={{
+                      height: '100px',
+                      width: '100px',
+                      marginTop: '-100px',
+                      marginLeft: '-30px'
+                    }}
+                  />
+                </div>
+              )}
               <div
-                className={`navigatio-item${index < page_id ? ' visited' : ''}`}
+                className={`navigatio-item${
+                  index + 1 < page_id ? ' visited' : ''
+                }`}
+                onClick={() => handleNavigationButton(index + 2)}
               >
                 {index + 2}
               </div>
