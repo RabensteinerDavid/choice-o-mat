25a3ff96aeb79b2c1cd2bf497809ba5030029040
Rabensteiner David <rabendavid02@gmail.com>
2024-05-31 00:29:14 +0200

    changed the top navigation

--- client/src/components/Links.jsx
+++ client/src/components/Links.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import '../style/links.css';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import '../style/links.css'
 
 class Links extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div className='top-header'> 
-                <Link to="/" className="nav-link">X</Link>
-                <Link /*to="/movies/list"*/ className="nav-link">?</Link></div>
-            </React.Fragment>
-        );
-    }
+  render () {
+    return (
+      <nav>
+        <div className='top-header'>
+        <Link to="/" className='nav-link cross'></Link>
+          <Link /*to="/movies/list"*/ className='nav-link'>?</Link>
+        </div>
+      </nav>
+      
+    )
+  }
 }
 
-export default Links;
+export default Links
