99cc0e8950fa5ace0ac166625f011fd9244f608c
Rabensteiner David <rabendavid02@gmail.com>
2024-06-07 18:31:24 +0200

    added dynamic result text

--- client/src/pages/questions/Result.js
+++ client/src/pages/questions/Result.js
@@ -4,9 +4,12 @@ import { Player } from '@lottiefiles/react-lottie-player'
 import { getResultLocalStorage } from '../../components/LoadQuestion'
 import '../../style/questions/result.css'
 import { Tooltip } from 'react-tooltip'
+import { getResults } from '../../api'
 
 function Result () {
   const [result, setResult] = useState(null)
+  const [resultTextMtd, setResultTextMtd] = useState('')
+  const [resultTextDa, setResultTextDa] = useState('')
 
   useEffect(() => {
     async function fetchResult () {
@@ -20,6 +23,29 @@ function Result () {
 
     fetchResult()
   }, [])
+
+  useEffect(() => {
+    const fetchDataQuestionTypes = async () => {
+      try {
+        const response = await getResults()
+
+        response.data.data.forEach(element => {
+          const rangeMin = parseInt(element.range.split('-')[0])
+          const rangeMax = parseInt(element.range.split('-')[1])
+          console.log(element.range)
+          if (rangeMin <= result.mtd && result.mtd <= rangeMax) {
+            setResultTextMtd(element.text)
+          }
+          if (rangeMin <= result.da && result.da <= rangeMax) {
+            setResultTextDa(element.text)
+          }
+        })
+      } catch (error) {
+        console.error('Error fetching questions:', error)
+      }
+    }
+    fetchDataQuestionTypes()
+  }, [result])
 
   function MoreInformation () {
     return (
@@ -46,12 +72,7 @@ function Result () {
               <div className='mtd bigger'>
                 <div className='mtd-wrapper'>
                   <h1>MTD</h1>
-                  <p className='result-text'>
-                    Lorem ipsum dolor sit amet consectetur. Massa leo blandit
-                    tincidunt aenean sit egestas. Est rhoncus sed habitasse sit.
-                    Imperdiet porttitor tempor imperdiet sit quam tempus ornare.
-                    Fermentum nibh a quisque ullamcorper amet.
-                  </p>
+                  <p className='result-text'>{resultTextMtd}</p>
                   <MoreInformation />
                 </div>
                 <a className='percentage start' id='mtd-tooltip'>
@@ -65,12 +86,7 @@ function Result () {
               <div className='da smaller'>
                 <div className='da-wrapper'>
                   <h1>DA</h1>
-                  <p className='result-text'>
-                    Lorem ipsum dolor sit amet consectetur. Massa leo blandit
-                    tincidunt aenean sit egestas. Est rhoncus sed habitasse sit.
-                    Imperdiet porttitor tempor imperdiet sit quam tempus ornare.
-                    Fermentum nibh a quisque ullamcorper amet.
-                  </p>
+                  <p className='result-text'>{resultTextDa}</p>
                   <MoreInformation />
                 </div>
                 <a className='percentage end' id='da-tooltip'>
@@ -87,12 +103,7 @@ function Result () {
               <div className='da bigger'>
                 <div className='da-wrapper'>
                   <h1>DA</h1>
-                  <p className='result-text'>
-                    Lorem ipsum dolor sit amet consectetur. Massa leo blandit
-                    tincidunt aenean sit egestas. Est rhoncus sed habitasse sit.
-                    Imperdiet porttitor tempor imperdiet sit quam tempus ornare.
-                    Fermentum nibh a quisque ullamcorper amet.
-                  </p>
+                  <p className='result-text'>{resultTextDa}</p>
                   <MoreInformation />
                 </div>
                 <a className='percentage start' id='da-tooltip'>
@@ -106,12 +117,7 @@ function Result () {
               <div className='mtd smaller'>
                 <div className='mtd-wrapper'>
                   <h1>MTD</h1>
-                  <p className='result-text'>
-                    Lorem ipsum dolor sit amet consectetur. Massa leo blandit
-                    tincidunt aenean sit egestas. Est rhoncus sed habitasse sit.
-                    Imperdiet porttitor tempor imperdiet sit quam tempus ornare.
-                    Fermentum nibh a quisque ullamcorper amet.
-                  </p>
+                  <p className='result-text'>{resultTextMtd}</p>
                   <MoreInformation />
                 </div>
                 <a className='percentage end' id='mtd-tooltip'>
