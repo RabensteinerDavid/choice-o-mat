9703d19aaef3fc3ddfdf82c8abd1e9aaf1cafa65
Rabensteiner David <rabendavid02@gmail.com>
2024-06-12 12:16:26 +0200

    ganzzahln result

--- client/src/components/LoadQuestion.js
+++ client/src/components/LoadQuestion.js
@@ -133,8 +133,8 @@ export const getResultLocalStorage = async () => {
         let daPercent = resultEachPercent.da
 
         result = {
-          da: ((daPercent / (mtdPercent + daPercent)) * 100).toFixed(2),
-          mtd: ((mtdPercent / (mtdPercent + daPercent)) * 100).toFixed(2)
+          da: ((daPercent / (mtdPercent + daPercent)) * 100).toFixed(0),
+          mtd: ((mtdPercent / (mtdPercent + daPercent)) * 100).toFixed(0)
         }
       }
     } else {
