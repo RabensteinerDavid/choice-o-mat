54260680d5cdffef63499d58294dc05aafd4424e
Rabensteiner David <rabendavid02@gmail.com>
2024-06-28 09:33:03 +0200

    prefilled questions

--- client/src/pages/questions/PrefilledResults.js
+++ client/src/pages/questions/PrefilledResults.js
@@ -1,27 +1,27 @@
 export const prefilledResults = [
   {
     "range": "0-20",
-    "textMtd": "You are at the beginner level. You're just starting out on your journey to becoming a skilled developer. Keep learning and practicing!",
-    "textDa": "You are at the beginner level. You're just starting out on your journey to becoming a skilled developer. Keep learning and practicing!"
+    "textMtd": "Medientechnik und -design scheint nicht dein Hauptinteresse zu sein. Du hast jedoch die Möglichkeit, mehr über dieses Feld zu lernen und zu sehen, ob es deine Neugier weckt. Nutze die Gelegenheit, um verschiedene Aspekte der Medientechnik zu erkunden und deine Interessen weiter zu entwickeln.",
+    "textDa": "Digital Arts scheint nicht dein Hauptinteresse zu sein. Du hast jedoch die Möglichkeit, mehr über dieses kreative Feld zu lernen und zu sehen, ob es deine Neugier weckt. Nutze die Gelegenheit, um verschiedene Aspekte der digitalen Kunst zu erkunden und deine Interessen weiter zu entwickeln."
   },
   {
     "range": "21-40",
-    "textMtd": "You are making progress. You have some experience and are getting more comfortable with coding concepts. Keep challenging yourself to improve!",
-    "textDa": "You are making progress. You have some experience and are getting more comfortable with coding concepts. Keep challenging yourself to improve!",
+    "textMtd": "Du zeigst ein gewisses Interesse an Medientechnik und -design. Obwohl es nicht dein Hauptfokus ist, findest du einige Aspekte dieses Feldes spannend. Setze dich weiter damit auseinander und entdecke, welche Bereiche dir besonders liegen könnten.",
+    "textDa": "Du zeigst ein gewisses Interesse an Digital Arts. Obwohl es nicht dein Hauptfokus ist, findest du einige kreative Aspekte dieses Feldes spannend. Setze dich weiter damit auseinander und entdecke, welche Bereiche dir besonders liegen könnten."
   },
   {
     "range": "41-60",
-    "textMtd": "You are at the intermediate level. You have a solid grasp of coding principles and are actively refining your skills. Keep pushing yourself to learn and grow!",
-    "textDa": "You are at the intermediate level. You have a solid grasp of coding principles and are actively refining your skills. Keep pushing yourself to learn and grow!",
+    "textMtd": "Du bist gut für Medientechnik und -design geeignet. Deine Interessen und Fähigkeiten sind sowohl technisch als auch kreativ ausgerichtet. Du wirst Freude daran haben, dich in Fächern wie Webentwicklung und Programmierung zu entfalten, aber auch kreative Herausforderungen anzunehmen. Du hast die Wahl, welcher Schwerpunkt besser zu dir passt.",
+    "textDa": "Du bist gut für Digital Arts geeignet. Deine Interessen und Fähigkeiten sind sowohl künstlerisch als auch technisch ausgerichtet. Du wirst Freude daran haben, dich in Fächern wie 3D Animation und Storytelling zu entfalten, aber auch technische Herausforderungen anzunehmen. Du hast die Wahl, welcher Schwerpunkt besser zu dir passt."
   },
   {
     "range": "61-80",
-    "textMtd": "You are at the advanced level. You have mastered many coding concepts and are adept at solving complex problems. Keep exploring new technologies and honing your expertise!",
-    "textDa": "You are at the advanced level. You have mastered many coding concepts and are adept at solving complex problems. Keep exploring new technologies and honing your expertise!",
+    "textMtd": "Du bist besonders gut für Medientechnik und -design geeignet. Du hast ein starkes technisches Verständnis und Interesse an Programmierung, Webentwicklung und Medientechnik. Gleichzeitig besitzt du kreative Fähigkeiten, die du in Fächern wie Fotografie und Videografie einsetzen kannst. Dieser Studiengang wird dir helfen, beide Seiten zu verbinden.",
+    "textDa": "Du bist besonders gut für Digital Arts geeignet. Du hast ein starkes künstlerisches Talent und Interesse an 3D Animation, Storytelling und kreativer Gestaltung. Gleichzeitig besitzt du technische Fähigkeiten, die dir in Fächern wie Webentwicklung zugutekommen. Dieser Studiengang wird dir helfen, beide Seiten zu verbinden."
   },
   {
     "range": "81-100",
-    "textMtd": "Congratulations! You are at the expert level. You possess extensive knowledge and skills in software development. Keep innovating and sharing your expertise with others!",
-    "textDa": "Congratulations! You are at the expert level. You possess extensive knowledge and skills in software development. Keep innovating and sharing your expertise with others!",
+    "textMtd": "Herzlichen Glückwunsch! Du bist hervorragend für den Studiengang Medientechnik und -design geeignet. Dein technisches Verständnis und deine Begeisterung für Programmierung und Datenanalyse sind beeindruckend. Du wirst es genießen, dich in Fächern wie Webentwicklung und Medientechnik zu vertiefen. Nutze diese Gelegenheit, um deine technischen und kreativen Fähigkeiten weiterzuentwickeln und zu fördern.",
+    "textDa": "Herzlichen Glückwunsch! Du bist hervorragend für den Studiengang Digital Arts geeignet. Deine künstlerischen Fähigkeiten und Interessen sind beeindruckend. Du wirst es genießen, dich in Fächern wie 3D Animation, Storytelling, Video und Rigging kreativ auszudrücken. Nutze diese Gelegenheit, um deine kreativen Visionen zu verwirklichen und deine künstlerischen Talente zu entfalten."
   }
 ];
