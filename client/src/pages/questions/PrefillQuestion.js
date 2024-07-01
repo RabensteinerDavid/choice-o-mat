183b39297e35fa65d148f3822b5e2529c3ea8d10
julis <s2210238053@fhooe.at>
2024-07-01 20:00:31 +0200

    added rihctige punkte

--- client/src/pages/questions/PrefillQuestion.js
+++ client/src/pages/questions/PrefillQuestion.js
@@ -5,22 +5,22 @@ export const prefilledQuestions = [
       "type": "Selection",
       "page": 1,
       "answers": [
-        { "text": "Podcast hören", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Social Media", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Die Gestaltung von Webseiten, damit sie gut aussehen und einfach zu benutzen sind.", "text": "Web Design", "points": { "da": 2, "mtd": 2 } },
-        { "text": "GIF's erstellen", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Das Erstellen von Bildern und Texten, um eine Botschaft zu vermitteln, wie in Logos, Postern und Werbung.", "text": "Grafikdesign", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Filme", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Das Bauen von dreidimensionalen Figuren und Objekten in 3D Programmen.", "text": "3D Modelling", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Poster/Flyer erstellen", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Videospiele, die Virtual Reality (VR)-Technologie nutzen, um  interaktive Erlebnisse in einer virtuellen Umgebung zu bieten.", "text": "VR Gaming", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": " Bearbeitung und Verbesserung von digitalen Bildern durch Techniken wie Retusche, Farbkorrektur und Filteranwendung.","text": "Bildbearbeitung", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Computer zusammenbauen", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Fotografie", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Cosplay / Kostümdesign", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Basteln / DIY", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Kunst und Technik der Gestaltung von Schrift, einschließlich Schriftartenwahl, Schriftgrößen und Layout, um Text lesbar und ansprechend zu machen.", "text": "Typografie", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Das Drucken von echten, dreidimensionalen Objekten, wie Spielzeug oder Ersatzteile, aus speziellen Druckern.", "text": "3D Druck", "points": { "da": 2, "mtd": 2 } }
+        { "text": "Podcast hören", "points": { "da": 5, "mtd": 5 } },
+        { "text": "Social Media", "points": { "da": 3, "mtd": 7 } },
+        { "explanation": "Die Gestaltung von Webseiten, damit sie gut aussehen und einfach zu benutzen sind.", "text": "Web Design", "points": { "da": 1, "mtd": 9 } },
+        { "text": "GIF's erstellen", "points": { "da": 5, "mtd": 5 } },
+        { "explanation": "Das Erstellen von Bildern und Texten, um eine Botschaft zu vermitteln, wie in Logos, Postern und Werbung.", "text": "Grafikdesign", "points": { "da": 6, "mtd": 4 } },
+        { "text": "Filme", "points": { "da": 8, "mtd": 2 } },
+        { "explanation": "Das Bauen von dreidimensionalen Figuren und Objekten in 3D Programmen.", "text": "3D Modelling", "points": { "da": 7, "mtd": 3 } },
+        { "text": "Poster/Flyer erstellen", "points": { "da": 6, "mtd": 4 } },
+        { "explanation": "Videospiele, die Virtual Reality (VR)-Technologie nutzen, um  interaktive Erlebnisse in einer virtuellen Umgebung zu bieten.", "text": "VR Gaming", "points": { "da": 2, "mtd": 8 } },
+        { "explanation": " Bearbeitung und Verbesserung von digitalen Bildern durch Techniken wie Retusche, Farbkorrektur und Filteranwendung.","text": "Bildbearbeitung", "points": { "da": 6, "mtd": 4 } },
+        { "text": "Computer zusammenbauen", "points": { "da": 0, "mtd": 10 } },
+        { "text": "Fotografie", "points": { "da": 6, "mtd": 4 } },
+        { "text": "Cosplay / Kostümdesign", "points": { "da": 5, "mtd": 5 } },
+        { "text": "Basteln / DIY", "points": { "da": 7, "mtd": 3 } },
+        { "explanation": "Kunst und Technik der Gestaltung von Schrift, einschließlich Schriftartenwahl, Schriftgrößen und Layout, um Text lesbar und ansprechend zu machen.", "text": "Typografie", "points": { "da": 3, "mtd": 7 } },
+        { "explanation": "Das Drucken von echten, dreidimensionalen Objekten, wie Spielzeug oder Ersatzteile, aus speziellen Druckern.", "text": "3D Druck", "points": { "da": 5, "mtd": 5 } }
       
     ]
     },
@@ -30,10 +30,10 @@ export const prefilledQuestions = [
       "type": "VerticalSlider",
       "page": 2,
       "answers": [
-        { "text": "Technisches Verständnis", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Ideen Entwicklung", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Künstlerische Fähigkeiten", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Praktisches Denken", "points": { "da": 2, "mtd": 2 } }
+        { "text": "Technisches Verständnis", "points": { "da": 2, "mtd": 8 } },
+        { "text": "Ideen Entwicklung", "points": { "da": 6, "mtd":  4} },
+        { "text": "Künstlerische Fähigkeiten", "points": { "da": 8, "mtd": 2 } },
+        { "text": "Praktisches Denken", "points": { "da": 2, "mtd": 8 } }
       ]
     },
     {
@@ -42,11 +42,11 @@ export const prefilledQuestions = [
       "type": "Carousel",
       "page": 3,
       "answers": [
-        { "explanation": "Leitet die IT-Abteilung und sorgt dafür, dass alle Computer und Netzwerke funktionieren.", "text": "IT-Manager", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Entwirft die Spielideen, Regeln und Levels für Videospiele.", "text": "Game Designer", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Erstellt dreidimensionale Bilder und Animationen für Filme und Spiele.", "text": "3D Artist", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Produziert und bearbeitet Geräusche und Musik für Medien wie Filme und Spiele.", "text": "Sound Designer", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Entwickelt das Aussehen und die Bedienbarkeit von Webseiten und Apps.", "text": "Front End Developer", "points": { "da": 2, "mtd": 2 } }
+        { "explanation": "Leitet die IT-Abteilung und sorgt dafür, dass alle Computer und Netzwerke funktionieren.", "text": "IT-Manager", "points": { "da": 0, "mtd": 10 } },
+        { "explanation": "Entwirft die Spielideen, Regeln und Levels für Videospiele.", "text": "Game Designer", "points": { "da": 8, "mtd": 2 } },
+        { "explanation": "Erstellt dreidimensionale Bilder und Animationen für Filme und Spiele.", "text": "3D Artist", "points": { "da": 8, "mtd": 2 } },
+        { "explanation": "Produziert und bearbeitet Geräusche und Musik für Medien wie Filme und Spiele.", "text": "Sound Designer", "points": { "da": 8, "mtd": 2 } },
+        { "explanation": "Entwickelt das Aussehen und die Bedienbarkeit von Webseiten und Apps.", "text": "Front End Developer", "points": { "da": 2, "mtd": 8 } }
       ]
     },
     {
@@ -55,14 +55,14 @@ export const prefilledQuestions = [
       "type": "ImageSelection",
       "page": 4,
       "answers": [
-        { "text": "Kino", "photo": "example.png", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Museum", "photo": "example.png", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Influencer", "photo": "example.png", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Gaming mit Headset ", "photo": "example.png", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Zeichnen", "photo": "example.png", "points": { "da": 2, "mtd": 2 } },
-        { "text": "It-Projekt zusammen", "photo": "example.png", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Programmieren", "photo": "example.png", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Tagebuch schreiben", "photo": "example.png", "points": { "da": 2, "mtd": 2 } }
+        { "text": "Kino", "photo": "example.png", "points": { "da": 6, "mtd": 4 } },
+        { "text": "Museum", "photo": "example.png", "points": { "da": 5, "mtd": 5 } },
+        { "text": "Influencer", "photo": "example.png", "points": { "da": 3, "mtd": 7 } },
+        { "text": "Gaming mit Headset ", "photo": "example.png", "points": { "da": 3, "mtd": 7 } },
+        { "text": "Zeichnen", "photo": "example.png", "points": { "da": 9, "mtd": 1 } },
+        { "text": "It-Projekt zusammen", "photo": "example.png", "points": { "da": 1, "mtd": 9 } },
+        { "text": "Programmieren", "photo": "example.png", "points": { "da": 1, "mtd": 9 } },
+        { "text": "Tagebuch schreiben", "photo": "example.png", "points": { "da": 7, "mtd": 3 } }
         
       ]
     },
@@ -72,11 +72,12 @@ export const prefilledQuestions = [
       "type": "OrderSelection",
       "page": 5,
       "answers": [
-        { "explanation": "Erstellung und Kombination von Text, Bildern, Audio und Video zu Inhalten, wie z.B. in Werbefilmen.", "text": "Multimedia Produktion", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": " Immersive 3D-Umgebung mit einer speziellen Brille.", "text": "Virtual Reality", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Erstellen eines digitalen Skeletts für 3D-Modelle,  damit man diese animieren kann.", "text": "3D Rigging", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Gestaltung ansprechender und benutzerfreundlicher visueller Elemente für verschrienste Produkte. ", "text": "Visual Design", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Grundidee und Design eines Videospiels, , einschließlich der Handlung, Spielmechaniken und Zielgruppe.", "text": "Game Concept", "points": { "da": 2, "mtd": 2 } }
+        { "explanation": "Erstellung und Kombination von Text, Bildern, Audio und Video zu Inhalten, wie z.B. in Werbefilmen.", "text": "Multimedia Produktion", "points": { "da": 2, "mtd": 8 } },
+        { "explanation": "Immersive 3D-Umgebung mit einer speziellen Brille.", "text": "Virtual Reality", "points": { "da": 4, "mtd": 6 } },
+        { "explanation": "Erstellen eines digitalen Skeletts für 3D-Modelle,  damit man diese animieren kann.", "text": "3D Rigging", "points": { "da": 7, "mtd": 3 } },
+        { "explanation": "Gestaltung ansprechender und benutzerfreundlicher visueller Elemente für verschiedenste Produkte. ", "text": "Visual Design", "points": { "da": 3, "mtd": 7 } },
+        { "explanation": "Grundidee und Design eines Videospiels, einschließlich der Handlung, Spielmechaniken und Zielgruppe.", "text": "Game Concept", "points": { "da": 8, "mtd": 2 } }
+        //wie vergebe ich hier punkte? wird das erste mal irgwas und dann weiter?
       ]
     },
     {
@@ -85,9 +86,10 @@ export const prefilledQuestions = [
       "type": "HorizontalSlider",
       "page": 6,
       "answers": [
-        { "text": "Organisatorisches Talent", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Analytische Fähigkeiten", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Eigenvermarktung", "points": { "da": 2, "mtd": 2 } }
+        { "text": "Organisatorisches Talent", "points": { "da": 5, "mtd": 5 } },
+        { "text": "Analytische Fähigkeiten", "points": { "da": 2, "mtd": 8 } },
+        { "text": "Eigenvermarktung", "points": { "da": 8, "mtd": 2} }
+        //wird auch wieder multipliziert oder?
       ]
     },
     {
@@ -96,14 +98,14 @@ export const prefilledQuestions = [
       "type": "DragnDrop",
       "page": 7,
       "answers": [
-        { "explanation": "Erstellen und Programmieren von Webseiten.", "text": "Webentwicklung", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Gestaltung der Benutzererfahrung und -oberfläche.Gestaltung der Benutzererfahrung und -oberfläche.", "text": "UX/UI Design", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Film Sound", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Character Animation", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Erstellen und Bearbeiten von Videos.", "text": "Video Design", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Auswerten und Interpretieren von Daten.", "text": "Datenanalyse", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Verwaltung und Organisation eines Unternehmens.", "text": "Business Managment", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Grafische Gestaltung von Videospielen.", "text": "Game Art", "points": { "da": 2, "mtd": 2 } }
+        { "explanation": "Erstellen und Programmieren von Webseiten.", "text": "Webentwicklung", "points": { "da": 1, "mtd": 9 } },
+        { "explanation": "Gestaltung der Benutzererfahrung und -oberfläche.Gestaltung der Benutzererfahrung und -oberfläche.", "text": "UX/UI Design", "points": { "da": 2, "mtd": 8 } },
+        { "text": "Film Sound", "points": { "da": 9, "mtd": 1 } },
+        { "text": "Character Animation", "points": { "da": 9, "mtd": 1 } },
+        { "explanation": "Erstellen und Bearbeiten von Videos.", "text": "Video Design", "points": { "da": 7, "mtd": 3 } },
+        { "explanation": "Auswerten und Interpretieren von Daten.", "text": "Datenanalyse", "points": { "da": 0, "mtd": 10 } },
+        { "explanation": "Verwaltung und Organisation eines Unternehmens.", "text": "Business Managment", "points": { "da": 5, "mtd": 5 } },
+        { "explanation": "Grafische Gestaltung von Videospielen.", "text": "Game Art", "points": { "da": 8, "mtd": 2 } }
       ]
     },
     {
@@ -112,12 +114,12 @@ export const prefilledQuestions = [
       "type": "ThisOrThatPicture",
       "page": 8,
       "answers": [
-        { "text": "Mobile App", "points": { "da": 2, "mtd": 2 } },
-        { "text": "3D Animationsfilm", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Stopmotion Film", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Musikvideo", "points": { "da": 2, "mtd": 2 } },
-        { "text": "VR Game", "points": { "da": 2, "mtd": 2 } },
-        { "text": "2D Animationsfilm", "points": { "da": 2, "mtd": 2 } }
+        { "text": "Mobile App", "points": { "da": 1, "mtd": 9 } },
+        { "text": "3D Animationsfilm", "points": { "da": 9, "mtd": 1 } },
+        { "text": "Stopmotion Film", "points": { "da": 9, "mtd": 1 } },
+        { "text": "Musikvideo", "points": { "da": 3, "mtd": 7 } },
+        { "text": "VR Game", "points": { "da": 2, "mtd": 8 } },
+        { "text": "2D Animationsfilm", "points": { "da": 9, "mtd": 1 } }
       ]
     },
     {
@@ -126,10 +128,10 @@ export const prefilledQuestions = [
       "type": "ThisOrThat",
       "page": 9,
       "answers": [
-        { "text": "Ich spiele gerne Videospiele und bin daran interessiert zu erfahren, wie sie entwickelt werden.", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "HTML-Code: Grundlegende Sprache zur Strukturierung von Webseiten.", "text": "Ich würde gerne HTML-Code verstehen.", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Ich würde gerne Geschichten durch visuelle Medien wie Animation oder Videoproduktion erzählen.", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Ich interessiere mich wie man die perfekten Charaktere zu einem Spiel kreieren kann..", "points": { "da": 2, "mtd": 2 } },
+        { "text": "Ich spiele gerne Videospiele und bin daran interessiert zu erfahren, wie sie entwickelt werden.", "points": { "da": 3, "mtd": 7 } },
+        { "explanation": "HTML-Code: Grundlegende Sprache zur Strukturierung von Webseiten.", "text": "Ich würde gerne HTML-Code verstehen.", "points": { "da": 4, "mtd": 6 } },
+        { "text": "Ich würde gerne Geschichten durch visuelle Medien wie Animation oder Videoproduktion erzählen.", "points": { "da": 8, "mtd": 2 } },
+        { "text": "Ich interessiere mich wie man die perfekten Charaktere zu einem Spiel kreieren kann.", "points": { "da": 9, "mtd": 1 } },
       ]
     },
     {
@@ -138,12 +140,12 @@ export const prefilledQuestions = [
       "type": "ChoiceRole",
       "page": 10,
       "answers": [
-        { "text": "Videoschnitt", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Drehbuch Autor/in", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Project Manager/in", "points": { "da": 2, "mtd": 2 } },
-        { "explanation": "Person, die das Design und die Herausforderungen von Spielebenen entwirft.", "text": "Level Planner", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Audio Techniker/in", "points": { "da": 2, "mtd": 2 } },
-        { "text": "Spezial Effekte", "points": { "da": 2, "mtd": 2 } }
+        { "text": "Videoschnitt", "points": { "da": 7, "mtd": 3 } },
+        { "text": "Drehbuch Autor/in", "points": { "da": 9, "mtd": 1 } },
+        { "text": "Project Manager/in", "points": { "da": 5, "mtd": 5 } },
+        { "explanation": "Person, die das Design und die Herausforderungen von Spielebenen entwirft.", "text": "Level Planner", "points": { "da": 4, "mtd": 6} },
+        { "text": "Audio Techniker/in", "points": { "da": 4, "mtd": 6 } },
+        { "text": "Spezial Effekte", "points": { "da": 6, "mtd": 4 } }
       ]
     }
   ];
